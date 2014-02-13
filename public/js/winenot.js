7e7ec7e68829ab96223f9b0e3bb8a203b6d78faa
PickL <bchou1@uci.edu>
2014-02-13 07:50:54 -0800

    Removed mongoose crap and setup ignores

--- public/js/winenot.js
+++ public/js/winenot.js
@@ -6,6 +6,28 @@
 
 	});
 
-	//var wineModel = Wine();
+	window.wine = new Wine({
+      "name": "Merlot",
+      "type": "Medium Red",
+      "color": "b42424",
+      "fruit": [
+        "Cherry",
+        "Raspberry",
+        "Plum"
+      ],
+      "other": [
+        "Graphite",
+        "Cedar",
+        "Tobacco",
+        "Vanilla",
+        "Clove",
+        "Mocha"
+      ],
+      "oak": "Yes. Usually medium oak aging",
+      "tannin": "Medium",
+      "acidity": "Medium",
+      "abv": "12% - 15%",
+      "region": ["france", "italy", "us", "australia", "chile", "argentina"]
+    });
 
 })(jQuery);
