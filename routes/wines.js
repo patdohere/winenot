77fb94edc169fb2503e5d9965ceca60dfb0fd056
Patrick Do <patdohere@gmail.com>
2014-02-13 23:30:31 -0800

    Starter mongodb code

--- routes/wines.js
+++ routes/wines.js
@@ -1,87 +1,19 @@
-var mongo = require('mongodb');
-
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
-
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('winedb', server, {safe: true});
-
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'winedb' database");
-        db.collection('wines', {safe:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
-                populateDB();
-            }
-        });
-    }
-});
-
 exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving wine: ' + id);
-    db.collection('wines', function(err, collection) {
-        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-            res.send(item);
-        });
-    });
+  res.send("findById");
 };
 
 exports.findAll = function(req, res) {
-    db.collection('wines', function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.send(items);
-        });
-    });
+  res.send("findAll");
 };
 
 exports.add = function(req, res) {
-    var wine = req.body;
-    console.log('Adding wine: ' + JSON.stringify(wine));
-    db.collection('wines', function(err, collection) {
-        collection.insert(wine, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
-    });
-}
+  res.send("add");
+};
 
 exports.update = function(req, res) {
-    var id = req.params.id;
-    var wine = req.body;
-    delete wine._id;
-    console.log('Updating wine: ' + id);
-    console.log(JSON.stringify(wine));
-    db.collection('wines', function(err, collection) {
-        collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
-            if (err) {
-                console.log('Error updating wine: ' + err);
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('' + result + ' document(s) updated');
-                res.send(wine);
-            }
-        });
-    });
-}
+  res.send("update");
+};
 
 exports.delete = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting wine: ' + id);
-    db.collection('wines', function(err, collection) {
-        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred - ' + err});
-            } else {
-                console.log('' + result + ' document(s) deleted');
-                res.send(req.body);
-            }
-        });
-    });
-}
+  res.send("delete");
+};
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var express = require('express'),
-    path = require('path'),
-    http = require('http'),
-    io = require('socket.io'),
-    wine = require('./routes/wines');
+  path = require('path'),
+  http = require('http'),
+  io = require('socket.io'),
+  wine = require('./routes/wines');
 
 var app = express();
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 8080);
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser())
-    app.use(express.static(path.join(__dirname, 'public')));
+  app.set('port', process.env.PORT || 8080);
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser())
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 var server = http.createServer(app);
 
 server.listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
 
 app.get('/wines', wine.findAll);
